80d768595c26a7dc96e3cb02c180c94cae116780
Hack Reactor Student <student@hackreactor.com>
2017-08-14 20:02:06 -0700

    Working on parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,32 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+
+  
+
+  // var index = 0;
+  
+  // if (json.length === index)
+
+  // string
+  // number
+  // object
+  // array
+  // true
+  // false
+  // null
 };
+
+var value = function(index, json) {
+
+  // if (json[index] === )
+};
+
+//we see a quote
+//we go to string function while increasing index
+// if(JSON[index] === '"')
+// var str = ";
+// while (JSON[index] !== '"') {
+//  str = str.concat(json[index]);
+//  index++;
+//}
